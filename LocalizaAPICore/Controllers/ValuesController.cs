a34e421dfaa9e7e0d461cb7596411c46ef82c8f8
Leonardo Oliveira <oliverbhz@gmail.com>
2020-08-12 23:33:09 -0300

    Create branch dev

--- LocalizaAPICore/Controllers/ValuesController.cs
+++ LocalizaAPICore/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace LocalizaAPICore.Controllers
 		[HttpGet]
 		public ActionResult<IEnumerable<string>> Get()
 		{
-			return new string[] { "value1", "value2" };
+			return new string[] { };
 		}
 
 		// GET api/values/5
